40c11344331da9f2d285bdd8747bb02871e456f2
AMADOU <developpeur01@icloud.com>
2026-08-15 23:13:18 +0000

    Ajout de la fonctionnalité produit

--- gestion_commandes/models/produit.py
+++ gestion_commandes/models/produit.py
@@ -1,1 +1,15 @@
 
+# models/produit.py
+class Produit:
+    """Classe représentant un produit dans le système."""
+
+    def __init__(self, reference, designation, prix_unitaire, stock, id=None, date_creation=None):
+        self.id = id
+        self.reference = reference
+        self.designation = designation
+        self.prix_unitaire = prix_unitaire
+        self.stock = stock
+        self.date_creation = date_creation
+
+    def __str__(self):
+        return f"[{self.reference}] {self.designation} - Prix: {self.prix_unitaire} FCFA | Stock: {self.stock}"
